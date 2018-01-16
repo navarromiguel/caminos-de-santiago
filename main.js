91084f0341e169666efcc44b8fbf2400daa6719e
mikelote <migue93@gmail.com>
2018-01-16 19:34:56 +0100

    Elevation added

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var geocoder;
 var directionsDisplay;
 var infowindow;
 var directionsService;;
+// Load the Visualization API and the columnchart package.
+// google.load("visualization", "1", {packages: ["columnchart"]});
 
 var map;
 function initMap() {
@@ -11,127 +13,20 @@ function initMap() {
 	directionsService = new google.maps.DirectionsService();
 	directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true, draggable: localStorage.getItem("edit_mode") == "true", preserveViewport: true});
 	geocoder = new google.maps.Geocoder();
+	// Create an ElevationService.
+    elevator = new google.maps.ElevationService();
 	var miLatlng = new google.maps.LatLng(39.5, -3);
 	var misOpciones = {
-	  center: miLatlng,
-	  zoom: 6,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
+		center: miLatlng,
+	  	zoom: 6,
+	  	mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-  map = new google.maps.Map(document.getElementById("map_container"), misOpciones);
-  var WMS_ADMIN = new google.maps.ImageMapType({
-	getTileUrl: function(coord, zoom) {
-		 var proj = map.getProjection();
-		 var zfactor = Math.pow(2, zoom);
-		 // Obtiene las coordenadas Long Lat
-		 var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
-		 var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
-
-		 //crea la cadena del Bounding box
-		 var bbox = (top.lng()) + "," + (bot.lat()) + "," + (bot.lng()) + "," + (top.lat());
-		 //URL del WMS
-		 var url = "http://www.ign.es/wms-inspire/camino-santiago";
-		 url += "?REQUEST=GetMap"; //Operación WMS
-		 url += "&SERVICE=WMS"; //servicio WMS
-		 url += "&VERSION=1.1.1"; //Versión WMS
-		 url += "&LAYERS=" + "AU.AdministrativeUnit"; //Capas WMS
-		 url += "&FORMAT=image/png"; //Formato WMS
-		 url += "&BGCOLOR=0xFFFFFF"; //Color de fondo
-		 url += "&TRANSPARENT=TRUE"; //Transparencia activada
-		 url += "&SRS=EPSG:4326"; //establece WGS84
-		 url += "&BBOX=" + bbox; // Establece el bounding box
-		 url += "&WIDTH=256"; //Tamaño de tesela en google
-		 url += "&HEIGHT=256";
-		 return url; // devuelve la URL para la tesela
-	},
-	tileSize: new google.maps.Size(256, 256),
-	isPng: true
-  });
- var CAPA_MADRID = new google.maps.ImageMapType({
-	getTileUrl: function(coord, zoom) {
-		 var proj = map.getProjection();
-		 var zfactor = Math.pow(2, zoom);
-		 // Obtiene las coordenadas Long Lat
-		 var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
-		 var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
-
-		 //crea la cadena del Bounding box
-		 var bbox = (top.lng()) + "," + (bot.lat()) + "," + (bot.lng()) + "," + (top.lat());
-		 //URL del WMS
-		 var url = "http://www.madrid.org/geoserver/mam/SIGI_MA_CAMINO_SANTIAGO/wms";
-		 url += "?REQUEST=GetMap"; //Operación WMS
-		 url += "&SERVICE=WMS"; //servicio WMS
-		 url += "&VERSION=1.1.1"; //Versión WMS
-		 url += "&LAYERS=" + "SIGI_MA_CAMINO_SANTIAGO"; //Capas WMS
-		 url += "&FORMAT=image/png"; //Formato WMS
-		 url += "&BGCOLOR=0xFFFFFF"; //Color de fondo
-		 url += "&TRANSPARENT=TRUE"; //Transparencia activada
-		 url += "&SRS=EPSG:4326"; //establece WGS84
-		 url += "&BBOX=" + bbox; // Establece el bounding box
-		 url += "&WIDTH=256"; //Tamaño de tesela en google
-		 url += "&HEIGHT=256";
-		 return url; // devuelve la URL para la tesela
-	},
-	tileSize: new google.maps.Size(256, 256),
-	isPng: true
-  });
-var CAPA_CIUDADES = new google.maps.ImageMapType({
-	getTileUrl: function(coord, zoom) {
-		 var proj = map.getProjection();
-		 var zfactor = Math.pow(2, zoom);
-		 // Obtiene las coordenadas Long Lat
-		 var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
-		 var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
-
-		 var bbox = (top.lng()) + "," + (bot.lat()) + "," + (bot.lng()) + "," + (top.lat());
-		 //crea la cadena del Bounding box
-		 var url = "http://www.ign.es/wms-inspire/camino-santiago";
-		 //URL del WMS
-		 url += "?REQUEST=GetMap"; //Operación WMS
-		 url += "&SERVICE=WMS"; //servicio WMS
-		 url += "&VERSION=1.1.1"; //Versión WMS
-		 url += "&LAYERS=" + "GN.GeographicalNames"; //Capas WMS
-		 url += "&FORMAT=image/png"; //Formato WMS
-		 url += "&BGCOLOR=0xFFFFFF"; //Color de fondo
-		 url += "&TRANSPARENT=TRUE"; //Transparencia activada
-		 url += "&SRS=EPSG:4326"; //establece WGS84
-		 url += "&BBOX=" + bbox; // Establece el bounding box
-		 url += "&WIDTH=256"; //Tamaño de tesela en google
-		 url += "&HEIGHT=256";
-		 return url; // devuelve la URL para la tesela
-	},
-	tileSize: new google.maps.Size(256, 256),
-	isPng: true
-  });
-var CAPA_CAMINOS = new google.maps.ImageMapType({
-	getTileUrl: function(coord, zoom) {
-		 var proj = map.getProjection();
-		 var zfactor = Math.pow(2, zoom);
-		 // Obtiene las coordenadas Long Lat
-		 var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
-		 var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
-
-		 var bbox = (top.lng()) + "," + (bot.lat()) + "," + (bot.lng()) + "," + (top.lat());
-		 //crea la cadena del Bounding box
-		 var url = "http://www.ign.es/wms-inspire/camino-santiago";
-		 //URL del WMS
-		 url += "?REQUEST=GetMap"; //Operación WMS
-		 url += "&SERVICE=WMS"; //servicio WMS
-		 url += "&VERSION=1.1.1"; //Versión WMS
-		 url += "&LAYERS=" + "PS.ProtectedSite"; //Capas WMS
-		 url += "&FORMAT=image/png"; //Formato WMS
-		 url += "&STYLES=PS.ProtectedSite.Default";
-		 url += "&BGCOLOR=0xFFFFFF"; //Color de fondo
-		 url += "&TRANSPARENT=TRUE"; //Transparencia activada
-		 url += "&SRS=EPSG:4326"; //establece WGS84
-		 url += "&BBOX=" + bbox; // Establece el bounding box
-		 url += "&WIDTH=256"; //Tamaño de tesela en google
-		 url += "&HEIGHT=256";
-		 return url; // devuelve la URL para la tesela
-	},
-	tileSize: new google.maps.Size(256, 256),
-	isPng: true
-  });
-	//Añade capaS WMS
+  	map = new google.maps.Map(document.getElementById("map_container"), misOpciones);
+  	// Crea y añade capaa WMS
+  	var WMS_ADMIN = createLayer("http://www.ign.es/wms-inspire/camino-santiago", "AU.AdministrativeUnit");
+ 	var CAPA_MADRID = createLayer("http://www.madrid.org/geoserver/mam/SIGI_MA_CAMINO_SANTIAGO/wms", "SIGI_MA_CAMINO_SANTIAGO");
+	var CAPA_CIUDADES = createLayer("http://www.ign.es/wms-inspire/camino-santiago", "GN.GeographicalNames");
+	var CAPA_CAMINOS = createLayer("http://www.ign.es/wms-inspire/camino-santiago", "PS.ProtectedSite");
  	map.overlayMapTypes.push(CAPA_MADRID); 
  	map.overlayMapTypes.push(WMS_ADMIN); 
  	map.overlayMapTypes.push(CAPA_CIUDADES); 
@@ -139,44 +34,47 @@ var CAPA_CAMINOS = new google.maps.ImageMapType({
 
  	if(localStorage.getItem("way"))
  		initialize(map);
-  google.maps.event.addListener(map, 'click', function(event){
-  	if(localStorage.getItem("edit_mode") == "true")
-  		addMarker(new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()), true);
-  });
+  	google.maps.event.addListener(map, 'click', function(event){
+  		if(localStorage.getItem("edit_mode") == "true"){
+  			addMarker(new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()), event, true);
+  			drawPath();
+  		}
+  	});
 
   	// Autocompletado
   	var input = (document.getElementById('search'));
-	var types = document.getElementById('type-selector');
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 	var autocomplete = new google.maps.places.Autocomplete(input);
 	autocomplete.bindTo('bounds', map);
     autocomplete.addListener('place_changed', function() {
-	      var place = autocomplete.getPlace();
-	      if (!place.geometry) {
+		var place = autocomplete.getPlace();
+	    if (!place.geometry) {
 	        // User entered the name of a Place that was not suggested and
 	        // pressed the Enter key, or the Place Details request failed.
 	        window.alert("No existe la ubicación: '" + place.name + "'");
-	        return;
-	      }
+	    	return;
+	    }
 
-	      // If the place has a geometry, then present it on a map.
-	      if (place.geometry.viewport) {
-	        map.fitBounds(place.geometry.viewport);
-	      } else {
+	    // If the place has a geometry, then present it on a map.
+	    if (place.geometry.viewport) {
+	    	map.fitBounds(place.geometry.viewport);
+	    } else {
 	        map.setCenter(place.geometry.location);
-	        map.setZoom(17);  // Why 17? Because it looks good.
-	      }
+	        map.setZoom(17); // Why 17? Because it looks good.
+	    }
 
-	      addMarker(place.geometry.location);
-	      var address = '';
-	      if (place.address_components) {
+	    addMarker(place.geometry.location);
+	    /*
+	    var address = '';
+	    if (place.address_components) {
 	        address = [
-	          (place.address_components[0] && place.address_components[0].short_name || ''),
-	          (place.address_components[1] && place.address_components[1].short_name || ''),
-	          (place.address_components[2] && place.address_components[2].short_name || '')
+	        	(place.address_components[0] && place.address_components[0].short_name || ''),
+	          	(place.address_components[1] && place.address_components[1].short_name || ''),
+	          	(place.address_components[2] && place.address_components[2].short_name || '')
 	        ].join(' ');
-	      }
+	   	}
+	   	*/
     });
 
 	// Evento encargado de añadir un camino nuevo
@@ -193,15 +91,15 @@ var CAPA_CAMINOS = new google.maps.ImageMapType({
 			if(edit_mode && id != "false" && id){
 				DB.updateWay(id, {
 						name: document.getElementById('nombre_camino').value,
-						markers: markers.map(function(marker){return marker.position;})
-					}).then(function(){
-						console.log("camino guardado");
-						window.location.href = 'index.html';
-					}).catch(function(error){console.log(error);});
+						markers: markers.map(function(marker){return {position: marker.position, content: marker.content}})
+				}).then(function(){
+					console.log("camino guardado");
+					window.location.href = 'index.html';
+				}).catch(function(error){console.log(error);});
 			} else {
 				DB.addWay({
 					name: document.getElementById('nombre_camino').value,
-					markers: markers.map(function(marker){return marker.position;})
+					markers: markers.map(function(marker){return {position: marker.position, content: marker.content}})
 				}).then(function(){
 					console.log("camino guardado");
 					window.location.href = 'index.html';
@@ -210,32 +108,161 @@ var CAPA_CAMINOS = new google.maps.ImageMapType({
 				});
 			}
 			localStorage.setItem("way", false);
-			
 		});
-		
 	});
 }
 
-function addMarker(location, edit_mode){
+function createLayer(_url, layer_name){
+	return new google.maps.ImageMapType({
+		getTileUrl: function(coord, zoom) {
+			var proj = map.getProjection();
+		 	var zfactor = Math.pow(2, zoom);
+		 	// Obtiene las coordenadas Long Lat
+		 	var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
+		 	var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
+
+		 	var bbox = (top.lng()) + "," + (bot.lat()) + "," + (bot.lng()) + "," + (top.lat());
+		 	//crea la cadena del Bounding box
+		 	var url = _url;
+		 	//URL del WMS
+		 	url += "?REQUEST=GetMap"; //Operación WMS
+		 	url += "&SERVICE=WMS"; //servicio WMS
+		 	url += "&VERSION=1.1.1"; //Versión WMS
+		 	url += "&LAYERS=" + layer_name; //Capas WMS
+		 	url += "&FORMAT=image/png"; //Formato WMS
+		 	url += "&STYLES=PS.ProtectedSite.Default";
+		 	url += "&BGCOLOR=0xFFFFFF"; //Color de fondo
+		 	url += "&TRANSPARENT=TRUE"; //Transparencia activada
+		 	url += "&SRS=EPSG:4326"; //establece WGS84
+		 	url += "&BBOX=" + bbox; // Establece el bounding box
+		 	url += "&WIDTH=256"; //Tamaño de tesela en google
+		 	url += "&HEIGHT=256";
+		 	return url; // devuelve la URL para la tesela
+		},
+		tileSize: new google.maps.Size(256, 256),
+		isPng: true
+  	});
+}
+
+function drawPath() {
+    // Create a new chart in the elevation_chart DIV.
+    chart = new google.visualization.ColumnChart(document.getElementById('elevation_chart'));
+    var path = markers.map(function(marker){return marker.position;})
+
+    // Create a PathElevationRequest object using this array.
+    // Ask for 256 samples along that path.
+    var pathRequest = {
+    	'path': path,
+    	'samples': 256
+    }
+
+    // Initiate the path request.
+    if(path.length >= 2)
+    	elevator.getElevationAlongPath(pathRequest, plotElevation);
+}
+
+// Takes an array of ElevationResult objects, draws the path on the map
+// and plots the elevation profile on a Visualization API ColumnChart.
+function plotElevation(results, status) {
+	if (status == google.maps.ElevationStatus.OK) {
+    	elevations = results;
+
+      	// Extract the elevation samples from the returned results
+      	// and store them in an array of LatLngs.
+      	var elevationPath = [];
+      	for (var i = 0; i < results.length; i++) {
+        	elevationPath.push(elevations[i].location);
+      	}
+
+      	// Display a polyline of the elevation path.
+      	var pathOptions = {
+        	path: elevationPath,
+        	strokeColor: '#0000CC',
+        	opacity: 0.4,
+        	map: map
+      	}
+     	// polyline = new google.maps.Polyline(pathOptions);
+
+      	// Extract the data from which to populate the chart.
+      	// Because the samples are equidistant, the 'Sample'
+      	// column here does double duty as distance along the
+      	// X axis.
+      	var data = new google.visualization.DataTable();
+      	data.addColumn('string', 'Sample');
+      	data.addColumn('number', 'Elevation');
+      	for (var i = 0; i < results.length; i++) {
+        	data.addRow(['', elevations[i].elevation]);
+      	}
+
+      	// Draw the chart using the data within its DIV. 
+      	document.getElementById('elevation_chart').style.display = 'block';
+      	chart.draw(data, {
+        	width: 640,
+        	height: 200,
+        	legend: 'none',
+        	titleY: 'Elevation (m)'
+      	});
+    }
+} 
+
+function getElevation(event, content, marker, txt) {
+    var locations = [];
+    // Retrieve the clicked location and push it on the array
+    var clickedLocation = event.latLng;
+    locations.push(clickedLocation);
+
+    // Create a LocationElevationRequest object using the array's one value
+    var positionalRequest = {
+    	'locations': locations
+    }
+
+    // Initiate the location request
+    elevator.getElevationForLocations(positionalRequest, function(results, status) {
+      	if (status == google.maps.ElevationStatus.OK) {
+			// Retrieve the first result
+        	if (results[0]) {
+        		var res = Math.round(results[0].elevation * 100) / 100
+        		marker.content = content + "La elevaci&oacute;n en este punto es de " + res + " metros."
+          		txt.data += ".  Altura: " + res + " metros."
+          		// Open an info window indicating the elevation at the clicked position
+          		infowindow.setContent('<div><strong>' + marker.content + '</strong></div>');
+        		//  infowindow.setPosition(clickedLocation);
+          		infowindow.open(map, marker);
+          		return results[0].elevation;
+        	} else {
+          		alert("No se han encontrado resultados");
+        	}
+      	} else {
+        	alert("El servicio de elevación ha fallado debido a: " + status);
+      	}
+    });
+}
+
+function addMarker(location, event, edit_mode){
 	console.log("adding marker...", location);
 	infowindow.close();
 	var marker = new google.maps.Marker({
 		position: location, 
 		map: map, 
 	});
+	//if(!localStorage.getItem("edit_mode") == "false"){
+	//	getElevation(event, content, marker);
+	//}
+	
 	markers.push(marker);
 	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent('<div><strong>' + marker.content + '</strong>');
+		infowindow.setContent(marker.content);
 		infowindow.open(map, marker);
 	});
 
 	var ul = document.getElementById('markers_container');
 	var li = document.getElementById('marker');
 	li = li.cloneNode(true);
+	var button = li.getElementsByTagName("button")[0];
 	li.removeAttribute("id");
 	if(!edit_mode)
-		li.getElementsByTagName("button")[0].className = " hide";
-	li.addEventListener('click', function(el){
+		button.className = " hide";
+	button.addEventListener('click', function(el){
 		var result = confirm("¿Seguro que desea eliminar?");
 		if(result){	
 			marker.setMap(null);
@@ -256,9 +283,13 @@ function addMarker(location, edit_mode){
 			li.appendChild(txt);
 			ul.appendChild(li);
 
-			marker.content = results[0]['formatted_address'];
-			infowindow.setContent('<div><strong>' + results[0]['formatted_address']+ '</strong>');
-			infowindow.open(map, markers[markers.length-1]);
+			marker.content = results[0]['formatted_address'] + "</br>";
+			//	infowindow.setContent('<div><strong>' + results[0]['formatted_address']+ '</strong>');
+			//	infowindow.open(map, markers[markers.length-1]);
+			if(typeof(event) == "boolean")
+				getElevation({latLng: marker.position}, marker.content, marker, txt);
+			else
+				getElevation(event, marker.content, marker, txt);
   		} else {
   			console.log("Error encoding location");
   		}
@@ -277,12 +308,13 @@ function initialize(){
 	DB.getWay(id).then(function(way){
 		document.getElementById('nombre_camino').value = way.name;
 		for (var i = 0; i < way.markers.length; i++) {
-			addMarker(way.markers[i], localStorage.getItem("edit_mode") == "true");
+			addMarker(way.markers[i].position, false, localStorage.getItem("edit_mode") == "true");
 		}
 		if(localStorage.getItem("edit_mode") == "false"){
 			document.getElementById("nombre_camino").readOnly = true;
 			document.getElementById("save_way").className += " hide";
 		}
+		drawPath();
 		loader.hide();
 	});
 }
@@ -297,16 +329,16 @@ function displayRoute(){
 	    waypoints: markers.slice(1, markers.length-1).map(function(el){
 	    	return {location: el.position};
 	    })
-	  };
-	  directionsService.route(request, function(result, status) {
+	};
+	directionsService.route(request, function(result, status) {
 	    if (status == 'OK') {
-	      directionsDisplay.setDirections(result);
-	      console.log("ruta creada", result);
+	      	directionsDisplay.setDirections(result);
+	      	console.log("ruta creada", result);
 	    } else {
 	    	console.log("fallo al crear ruta");
 	    }
-	  });
-	  directionsDisplay.setMap(map);
+	});
+	directionsDisplay.setMap(map);
 }
 
 // Redibuja la ruta
@@ -314,7 +346,4 @@ function resetRoute(){
 	directionsDisplay.setMap(null);
 	displayRoute();
 }
-
-console.log("db", db);
-
 
